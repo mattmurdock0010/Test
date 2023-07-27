6ee0d2c06972342a3b91105378d6b95d664199ac
mattmurdock0010 <106631710+mattmurdock0010@users.noreply.github.com>
2023-07-27 21:59:05 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="tr">
+<head>
+<style>* { font-family: tahoma; }</style>
+<script type="text/javascript">
+document.addEventListener('contextmenu', event => event.preventDefault());
+var tid = setInterval(function() { var bef = new Date().getTime();debugger;var af = new Date().getTime();if (af - bef > 100){clearInterval(tid);window.location.href = "/";} }, 100);
+</script>
+
+<script src="https://msplay.site/player/assets/devtools-detector/promise-polyfill.js"></script>
+<script src="https://msplay.site/player/assets/devtools-detector/devtools-detector.js"></script>
+<script type="text/javascript">
+!function() {
+	/*referans:box*/
+    navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) || (devtoolsDetector.addListener(function(t, e) {
+        t && (document.location.href = "https://msplay.site/player/no_video.html")
+    }), devtoolsDetector.lanuch())
+}();
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta http-equiv="content-language" content="TR"/>
+<title>Esaretin.Bedeli-The.Shawshank.Redemption.1994.TRDublaj.mkv</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<script type="text/javascript" src="//code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript" src="https://msplay.site/player/scripts.php?v=29"></script>
+
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+
+<style type="text/css">
+html,body{height:100%;width:100%;padding:0;margin:0;background-color:black}#player,#playerbase{position:fixed;width:100%;height:100%!important;left:0;top:0;z-index:10}.jw-rightclick{display: none !important}.jw-settings-menu{width: 150px !important;}.jw-breakpoint-7 .jw-settings-menu,.jw-breakpoint-6 .jw-settings-menu,.jw-breakpoint-5 .jw-settings-menu,.jw-breakpoint-4 .jw-settings-menu,.jw-breakpoint-3 .jw-settings-menu{max-height: 220px !important;height: 220px !important;}.jw-settings-content-item{font-size: 15px}.jw-settings-content-item.jw-settings-item-active{color: #35c2ff}.jw-text-elapsed,.jw-text-duration{font-size: 15px}.jw-slider-horizontal .jw-slider-container{height: 10px}.jw-knob{height: 16px;width: 16px}.rek{position:absolute;z-index:999998;float:left;left:50%;top:50%;height:auto;width:auto;transform:translate(-50%,-50%);background-color:rgba(0,0,0,.3)}.rek_close{z-index:999999;position:absolute;color:white;top:12px;right:3px;cursor:pointer;font-size:20px;font-weight:700;line-height:0;-webkit-transition-duration:0.4s;opacity:1}.rek_counter{z-index:999999;position:absolute;color:black;margin:auto;width:100%;bottom:8px;background-color:rgba(255,255,255,.5);font-family:tahoma;text-align:center;cursor:pointer;font-size:14px;font-weight:700;text-shadow:1px 1px 1px white,0 0 1em white,0 0 .2em black}.blockout{position: absolute;top: 10px;right: 10px;z-index: 999996;width: 45px;height: 45px;background-color: rgba(0,0,0,0.0);}
+</style>
+
+<script type="text/javascript">
+var player_base_url = "https://msplay.site/player";
+</script>
+
+</head>
+<body oncontextmenu="return false;">
+
+
+<div class="blockout" style="display:none;"></div>
+
+<div class="loader" style="">
+	<div class="page-loader__spinner"></div>
+</div>
+
+<div id="playerbase" style="display:none;"></div>
+
+<script type="text/javascript">
+var vhash = "c57168a952f5d46724cf35dfc3d48a7f";
+function fireload(vhash) {
+	FirePlayer(vhash, {"hostList":{"1":["stf1.zplay2.cf","stf1.zplay3.cf","stf1.zplay4.cf","stf1.zplay5.cf","stf1.zplay6.cf"],"2":["stf2.zplay7.cf","stf2.zplay8.cf"],"3":["stf3.zplay9.cf","stf3.zplay10.cf"],"4":["stf4.zplay11.cf","stf4.zplay13.cf"],"5":["stf5.zplay14.cf","stf5.zplay15.cf"],"6":["stf6.zplay16.cf","stf6.zplay17.cf"],"8":["stf8.zplay18.cf","stf8.zplay19.cf","stf8.zplay20.cf"]},"videoUrl":"\/cdn\/hls\/1da6e50ec814754044c9b04028d3a9e3\/master.txt","videoServer":"1","videoPlayer":"jwplayer","isJWPlayer8":true,"jwPlayerKey":"ksaKvjlJRbnrPXSGpuPVqfscYS9+rZFhulEcXvUQMbyWAmIQyyjPjZAQPLw=","jwPlayerURL":"https:\/\/msplay.site\/player\/assets\/jwplayer-8.12.5_\/jwplayer.js","logo":{"file":"","link":"","position":"top-left","hide":false},"tracks":[],"captions":{"fontSize":"20","fontfamily":"Tahoma"},"defaultImage":"https:\/\/msplay.site\/player\/hlswall3.jpg","SubtitleManager":false,"jwplayer8button1":true,"jwplayer8quality":true,"title":"Esaretin.Bedeli-The.Shawshank.Redemption.1994.TRDublaj.mkv","displaytitle":false,"rememberPosition":true,"advertising":[],"videoData":{"videoImage":null,"videoSources":[{"file":"https:\/\/1\/cdn\/hls\/1da6e50ec814754044c9b04028d3a9e3\/master.txt","label":"HD","type":"hls"}]}}, false);
+}
+
+$(function(){
+	$(document).ready(function() {
+		fireload(vhash);
+	});
+});
+</script>
+<script type="text/javascript">
+$(document).on({
+    "contextmenu": function(e) {
+        e.preventDefault();
+    }
+});
+</script>
+</body>
+</html>
